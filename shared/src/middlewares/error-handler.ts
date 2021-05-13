f300e8f4ab0401d01737b88ea6f7d76af68e9630
Naveed Fida <naveedfida01@gmail.com>
2021-05-13 20:30:04 +0500

    minor fix

--- shared/src/middlewares/error-handler.ts
+++ shared/src/middlewares/error-handler.ts
@@ -9,7 +9,7 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof CustomError) {
-    res.status(err.statusCode).send({
+    return res.status(err.statusCode).send({
       errors: err.toArray()
     });
   }
