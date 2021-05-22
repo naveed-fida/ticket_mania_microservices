9fdb5623b8e79bae87138419bf59238f1bfb5623
Naveed Fida <naveedfida01@gmail.com>
2021-05-22 20:04:50 +0500

    add show route for orders

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,21 @@
 import express, {Request, Response} from 'express';
+import {NotAuthorizedError, NotFoundError, requireAuth} from "@nf-ticket-mania/shared";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get('/api/orders/:id', (req: Request, res: Response) => {
-  res.send({});
+router.get('/api/orders/:id', requireAuth, async (req: Request, res: Response) => {
+  const order = await Order.findById(req.params.id).populate('ticket');
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+
+  if (order.userId !== req. currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+  res.send(order);
 });
 
 export { router as showOrderRouter };
