c7389dd3a7ae01812d1e50d2229b88a37ded84a8
Naveed Fida <naveedfida01@gmail.com>
2021-05-22 20:05:09 +0500

    add delete route for orders

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,24 @@
 import express, {Request, Response} from 'express';
+import {NotAuthorizedError, NotFoundError, requireAuth} from "@nf-ticket-mania/shared";
+import {Order, OrderStatus} from "../models/order";
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', (req: Request, res: Response) => {
-  res.send({});
+router.delete('/api/orders/:id', requireAuth, async (req: Request, res: Response) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+
+  if (order.userId !== req. currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+  order.status = OrderStatus.Cancelled;
+  await order.save();
+
+  res.sendStatus(204);
 });
 
 export { router as deleteOrderRouter };
