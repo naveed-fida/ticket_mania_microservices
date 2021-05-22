f061ed7a9155942f403dc8c3216994a1fb1510bb
Naveed Fida <naveedfida01@gmail.com>
2021-05-22 17:25:46 +0500

    update version number

--- orders/src/routes/create.ts
+++ orders/src/routes/create.ts
@@ -1,8 +1,23 @@
 import express, {Request, Response} from 'express';
+import {requireAuth, validateRequest} from '@nf-ticket-mania/shared';
+import {body} from "express-validator";
 
 const router = express.Router();
 
-router.post('/api/orders/', (req: Request, res: Response) => {
+const validators = [
+  body('ticketId')
+    .not()
+    .isEmpty()
+    .withMessage('Ticket ID must be provided!')
+]
+
+const middlewares = [
+  requireAuth,
+  ...validators,
+  validateRequest
+]
+
+router.post('/api/orders/', middlewares, (req: Request, res: Response) => {
   res.send({});
 });
 
