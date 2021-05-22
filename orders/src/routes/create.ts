c00d74792b832a099292ef601cc690a04e527e2f
Naveed Fida <naveedfida01@gmail.com>
2021-05-22 20:00:14 +0500

    add create route

--- orders/src/routes/create.ts
+++ orders/src/routes/create.ts
@@ -1,6 +1,11 @@
 import express, {Request, Response} from 'express';
-import {requireAuth, validateRequest} from '@nf-ticket-mania/shared';
+import {BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest} from '@nf-ticket-mania/shared';
 import {body} from "express-validator";
+
+import {Ticket} from "../models/ticket";
+import {Order} from "../models/order";
+
+const EXPIRATION_WINDOW_SECOND = 15 * 60;
 
 const router = express.Router();
 
@@ -17,8 +22,32 @@ const middlewares = [
   validateRequest
 ]
 
-router.post('/api/orders/', middlewares, (req: Request, res: Response) => {
-  res.send({});
+router.post('/api/orders/', middlewares, async (req: Request, res: Response) => {
+  const {ticketId} = req.body;
+
+  const ticket = await Ticket.findById(ticketId);
+  if (!ticket) {
+    throw new NotFoundError();
+  }
+
+  const isReserved = await ticket.isReserved();
+  if (isReserved) {
+    throw new BadRequestError('Ticket already taken!');
+  }
+
+  const expiration = new Date();
+  expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECOND);
+
+  const order = new Order({
+    userId: req.currentUser!.id,
+    status: OrderStatus.Created,
+    expiresAt: expiration,
+    ticket
+  });
+
+  await order.save();
+
+  res.status(201).send(order);
 });
 
 export { router as createOrderRouter };
