0f9da28e091476d4aaf55b66c150a4ac0cd21e96
Naveed Fida <naveedfida01@gmail.com>
2021-05-23 19:39:30 +0500

    refactor events in shared package

--- shared/src/events/event-types.ts
+++ shared/src/events/event-types.ts
@@ -1,4 +1,5 @@
 import {Subjects} from "./subjects";
+import {OrderStatus} from "./types/order-status";
 
 interface TicketData {
   id: string;
@@ -21,4 +22,28 @@ export interface TicketUpdatedEvent {
   subject: Subjects.TicketUpdated
   data: TicketData
 }
+
+export interface OrderCancelledEvent {
+  subject: Subjects.OrderCancelled,
+  data: {
+    id: string;
+    ticket: {
+      id: string;
+    }
+  }
+};
+
+export interface OrderCreatedEvent {
+  subject: Subjects.OrderCreated,
+  data: {
+    id: string;
+    status: OrderStatus;
+    userId: string;
+    expiresAt: string;
+    ticket: {
+      id: string;
+      price: number;
+    }
+  }
+};
 
--- shared/src/events/index.ts
+++ shared/src/events/index.ts
@@ -3,5 +3,3 @@ export * from './nats-connection';
 export * from './subjects';
 export * from './event-types';
 export * from './types/order-status'
-export * from './order-cancelled-event';
-export * from './order-created-event';
